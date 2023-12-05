8f00270aa721a445a83ff27531370f9f2e5a926c
Diego Katz <diekatz@gmail.com>
2023-12-05 18:52:50 -0300

    verificado y terminado

--- Program.cs
+++ Program.cs
@@ -10,6 +10,23 @@ namespace GetLenght
     {
         static void Main(string[] args)
         {
+            Double[,] matriz2D = { {1,2,3},
+                                    {4,5,6} };
+            ImprimirMatrizMulti(matriz2D);
+        }
+
+        static void ImprimirMatrizMulti(double[,] matriz2DPa)
+        {
+            int i, j;
+            for (i = 0; i < matriz2DPa.GetLength(0); i++)
+            {
+                Console.WriteLine("fila {0}", i);
+                for (j = 0; j < matriz2DPa.GetLength(1); j++)
+                {
+                    Console.WriteLine(matriz2DPa[i, j]);
+                }
+            }
         }
     }
+
 }
